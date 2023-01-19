9805837241038399116e7fd387998c218b4eba14
annalisapol <annapol2003@gmail.com>
2023-01-19 16:46:26 +0100

    funzione 1 e 2

--- pcb.c
+++ pcb.c
@@ -1,14 +1,19 @@
 #include "pcb.h"
 
-
-
 void initPcbs()
 {
-    
+    INIT_LIST_HEAD(&pcbFree);
+    pcbFree_h = &pcbFree;
+    for (int i = 1; i < MAXPROC; i++)
+    {
+        list_add(&pcbFree_table[i].p_list, pcbFree_h);
+    }
 }
 void freePcb(pcb_t *p)
 {
+    list_add_tail(&(p->p_list), pcbFree_h);
 }
+
 pcb_t *allocPcb()
 {
 }
--- pcb.h
+++ pcb.h
@@ -2,9 +2,9 @@
 #include "pandos_types.h"
 #include "list.h"
 
-
 pcb_t pcbFree_table[MAXPROC];
-list_head pcbFree_h;
+struct list_head pcbFree;
+struct list_head *pcbFree_h;
 
 // pcb double linked list functions
 /*
