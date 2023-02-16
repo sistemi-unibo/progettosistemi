86c6ce24321ba27deaf79a8de07e3e80f6a07d94
Nicole <nicolesabrina.marro@studio.unibo.it>
2023-02-16 19:08:14 +0100

    Correzioni hash functions

--- ash.c
+++ ash.c
@@ -1,7 +1,6 @@
 #include "ash.h"
 
-
-//aux function used to get a semaphore from the sem_h list given the key
+// aux function used to get a semaphore from the sem_h list given the key
 semd_t *getsem(int *key)
 {
     struct semd_t *x;
@@ -20,30 +19,23 @@ semd_t *getsem(int *key)
 int insertBlocked(int *semAdd, pcb_t *p)
 {
     semd_t *sem = getsem(semAdd);
-    if (sem != NULL)   //semaphore found, inserts the pcb in semprocq
+    if (sem != NULL) // semaphore found, inserts the pcb in semprocq
     {
         insertProcQ(&(sem->s_procq), p);
     }
     else
     {
-        if (list_empty(semdFree_h))  //no more free semaphores to allocate
+        if (list_empty(semdFree_h)) // no more free semaphores to allocate
         {
             return TRUE;
         }
-        else  //allocates a new semaphore
+        else // allocates a new semaphore
         {
             semd_t *new = semdFree_h->next;
-            list_del(semdFree_h->next);  //deletes the first entry from the list of free semaphores
-            /*
-            semd_t *newsem = container_of(new, semd_t, s_link); //pics the free pcb and creates a new one for the active sem list
-            */
-            hash_add(semd_h,&sem->s_link, semAdd);   //adds new sem to hashtable
-            insertProcQ(&(new->s_procq), p);
+            list_del(semdFree_h->next);             // deletes the first entry from the list of free semaphores
+            hash_add(semd_h, &new->s_link, semAdd); // adds new sem to hashtable
+            insertProcQ(&(new->s_procq), p);        // inserts pcb to tail of newsem procq list
             new->s_key = semAdd;
-            /*
-            insertProcQ(&(newsem->s_procq), p);   //inserts pcb to tail of newsem procq list
-            newsem->s_key = semAdd;
-            */
         }
     }
     p->p_semAdd = semAdd;
@@ -53,11 +45,15 @@ int insertBlocked(int *semAdd, pcb_t *p)
 pcb_t *removeBlocked(int *semAdd)
 {
     semd_t *sem = getsem(semAdd);
-    if (sem == NULL){
+    if (sem == NULL)
+    {
         return NULL;
-    }else{
-        struct pcb_t* pcbdel = removeProcQ(&(sem->s_procq));
-        if(list_empty(&(sem->s_procq))){
+    }
+    else
+    {
+        struct pcb_t *pcbdel = removeProcQ(&(sem->s_procq));
+        if (list_empty(&(sem->s_procq)))
+        {
             hash_del(&(sem->s_link));
             list_add(&(sem->s_freelink), semdFree_h);
         }
@@ -69,37 +65,53 @@ pcb_t *outBlocked(pcb_t *p)
 {
     semd_t *sem = p->p_semAdd;
 
-    if(sem==NULL)
+    if (sem == NULL)
+    {
         return NULL;
-
-    pcb_t *p_out = outProcQ(&(sem->s_procq), p); 
-    if (p_out == NULL ){
-        return NULL;
-    } else {
-        if(list_empty(&(sem->s_procq))){
-            hash_del(&(sem->s_link));
-            list_add(&(sem->s_freelink), semdFree_h);
+    }
+    else
+    {
+        pcb_t *p_out = outProcQ(&(sem->s_procq), p);
+        if (p_out == NULL)
+        {
+            return NULL;
         }
-        return p;
+        else
+        {
+            if (list_empty(&(sem->s_procq)))
+            {
+                hash_del(&(sem->s_link));
+                list_add(&(sem->s_freelink), semdFree_h);
+            }
+            return p;
+        }
     }
 }
 
-pcb_t *headBlocked(int *semAdd){
+pcb_t *headBlocked(int *semAdd)
+{
     semd_t *sem = getsem(semAdd);
-    if (sem == NULL){
+    if (sem == NULL)
+    {
         return NULL;
-    }else{
-        if(list_empty(&(sem->s_procq))){
+    }
+    else
+    {
+        if (list_empty(&(sem->s_procq)))
+        {
             return NULL;
-        }else{
+        }
+        else
+        {
             pcb_t *headp = headProcQ(&(sem->s_procq));
             return headp;
         }
     }
 }
-void initASH(){
+void initASH()
+{
     INIT_LIST_HEAD(semdFree_h);
-    for (int i = 0; i<MAXPROC; i++)
+    for (int i = 0; i < MAXPROC; i++)
     {
         list_add(&semd_table[i].s_freelink, semdFree_h);
     }
