03833949074687829ce293d395b6c821b259db54
Nicole <nicolesabrina.marro@studio.unibo.it>
2023-02-16 19:35:06 +0100

    creato i file e le strutture dati per i namespaces

---
+++ ns.h
@@ -0,0 +1,14 @@
+#ifndef NS_H
+#define ND_H
+
+#include "types.h"
+#include "pandos_types.h"
+#include "list.h"
+#include "pcb.h"
+#include "hashtable.h"
+
+nsd_t type_nsd[MAXPROC];
+list_head *type_nsFree_h;
+list_head *type_nsList_h;
+
+#endif
