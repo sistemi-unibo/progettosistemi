2f4ce67234dcf00f2ff1109e74331dd1e0c4d57a
3FrA3 <menarinif@gmail.com>
2023-02-17 23:36:09 +0100

    corretto getnaspaces

--- ns.c
+++ ns.c
@@ -7,6 +7,7 @@ static struct list_head type_nsList_h;
 void initNamespaces() //testata e funziona
 {
     INIT_LIST_HEAD(&type_nsFree_h);
+    INIT_LIST_HEAD(&type_nsList_h);
     for (int i = 0; i < MAXPROC; i++)
     {
         list_add(&type_nsd[i].n_link, &type_nsFree_h);
@@ -17,20 +18,6 @@ void initNamespaces() //testata e funziona
 
 nsd_t *getNamespace(pcb_t *p, int type)
 {
-    int flag = 0;
-    nsd_t* n = NULL;
-
-    struct list_head * new = &type_nsList_h;
-    while(new != NULL && !flag)
-    {
-        nsd_PTR ns = container_of(new, nsd_t, n_link);
-        if(ns->n_type == type) 
-        {
-            flag = 1;
-            n = ns;
-        }
-
-    }
-    return n;
+return p->namespaces[type];
 
 }
--- p1test.c
+++ p1test.c
@@ -253,88 +253,88 @@ int main(void) {
         freePcb(procp[i]);
 
 
-    /* check ASH */
-    initASH();
-    addokbuf("Initialized active semaphore hash   \n");
+    // /* check ASH */
+    // initASH();
+    // addokbuf("Initialized active semaphore hash   \n");
 
-    /* check removeBlocked and insertBlocked */
-    addokbuf("insertBlocked test #1 started  \n");
-    for (i = 10; i < MAXPROC; i++) {
-        procp[i] = allocPcb();
-        if (insertBlocked(&sem[i], procp[i]))
-            adderrbuf("insertBlocked(1): unexpected TRUE   ");
-    }
-    addokbuf("insertBlocked test #2 started  \n");
-    for (i = 0; i < 10; i++) {
-        procp[i] = allocPcb();
-        if (insertBlocked(&sem[i], procp[i]))
-            adderrbuf("insertBlocked(2): unexpected TRUE   ");
-    }
-
-    /* check if semaphore descriptors are returned to free list */
-    p = removeBlocked(&sem[11]);
-    if (insertBlocked(&sem[11], p))
-        adderrbuf("removeBlocked: fails to return to free list   ");
-
-    if (insertBlocked(&onesem, procp[9]) == FALSE)
-        adderrbuf("insertBlocked: inserted more than MAXPROC   ");
-
-    addokbuf("removeBlocked test started   \n");
-    for (i = 10; i < MAXPROC; i++) {
-        q = removeBlocked(&sem[i]);
-        if (q == NULL)
-            adderrbuf("removeBlocked: wouldn't remove   ");
-        if (q != procp[i])
-            adderrbuf("removeBlocked: removed wrong element   ");
-        if (insertBlocked(&sem[i - 10], q))
-            adderrbuf("insertBlocked(3): unexpected TRUE   ");
-    }
-    if (removeBlocked(&sem[11]) != NULL)
-        adderrbuf("removeBlocked: removed nonexistent blocked proc   ");
-    addokbuf("insertBlocked and removeBlocked ok   \n");
-
-    if (headBlocked(&sem[11]) != NULL)
-        adderrbuf("headBlocked: nonNULL for a nonexistent queue   ");
-    if ((q = headBlocked(&sem[9])) == NULL)
-        adderrbuf("headBlocked(1): NULL for an existent queue   ");
-    if (q != procp[9])
-        adderrbuf("headBlocked(1): wrong process returned   ");
-    p = outBlocked(q);
-    if (p != q)
-        adderrbuf("outBlocked(1): couldn't remove from valid queue   ");
-    q = headBlocked(&sem[9]);
-    if (q == NULL)
-        adderrbuf("headBlocked(2): NULL for an existent queue   ");
-    if (q != procp[19])
-        adderrbuf("headBlocked(2): wrong process returned   ");
-    p = outBlocked(q);
-    if (p != q)
-        adderrbuf("outBlocked(2): couldn't remove from valid queue   ");
-    p = outBlocked(q);
-    if (p != NULL)
-        adderrbuf("outBlocked: removed same process twice.");
-    if (headBlocked(&sem[9]) != NULL)
-        adderrbuf("out/headBlocked: unexpected nonempty queue   ");
-
-    for (i = 0; i < MAXPROC; i++)
-        freePcb(procp[i]);
-
-    addokbuf("headBlocked and outBlocked ok   \n");
-    addokbuf("ASH module ok   \n");
-
-    // /* check Namespaces */
-    //initNamespaces();
-    //addokbuf("Initialized Namespaces\n");
-
-    // /* check normal namespace (getNamespace) */
-    // addokbuf("getNamespace test #1 started  \n");
-    // for (i = 0; i < MAXPROC; i++) {
+    // /* check removeBlocked and insertBlocked */
+    // addokbuf("insertBlocked test #1 started  \n");
+    // for (i = 10; i < MAXPROC; i++) {
     //     procp[i] = allocPcb();
-    //     if (getNamespace(procp[i], NS_PID) != NULL)
-    //         adderrbuf("getNamespace(1): unexpected Namespace   ");
+    //     if (insertBlocked(&sem[i], procp[i]))
+    //         adderrbuf("insertBlocked(1): unexpected TRUE   ");
+    // }
+    // addokbuf("insertBlocked test #2 started  \n");
+    // for (i = 0; i < 10; i++) {
+    //     procp[i] = allocPcb();
+    //     if (insertBlocked(&sem[i], procp[i]))
+    //         adderrbuf("insertBlocked(2): unexpected TRUE   ");
     // }
 
-    // addokbuf("getNamespace test #1 ok\n");
+    // /* check if semaphore descriptors are returned to free list */
+    // p = removeBlocked(&sem[11]);
+    // if (insertBlocked(&sem[11], p))
+    //     adderrbuf("removeBlocked: fails to return to free list   ");
+
+    // if (insertBlocked(&onesem, procp[9]) == FALSE)
+    //     adderrbuf("insertBlocked: inserted more than MAXPROC   ");
+
+    // addokbuf("removeBlocked test started   \n");
+    // for (i = 10; i < MAXPROC; i++) {
+    //     q = removeBlocked(&sem[i]);
+    //     if (q == NULL)
+    //         adderrbuf("removeBlocked: wouldn't remove   ");
+    //     if (q != procp[i])
+    //         adderrbuf("removeBlocked: removed wrong element   ");
+    //     if (insertBlocked(&sem[i - 10], q))
+    //         adderrbuf("insertBlocked(3): unexpected TRUE   ");
+    // }
+    // if (removeBlocked(&sem[11]) != NULL)
+    //     adderrbuf("removeBlocked: removed nonexistent blocked proc   ");
+    // addokbuf("insertBlocked and removeBlocked ok   \n");
+
+    // if (headBlocked(&sem[11]) != NULL)
+    //     adderrbuf("headBlocked: nonNULL for a nonexistent queue   ");
+    // if ((q = headBlocked(&sem[9])) == NULL)
+    //     adderrbuf("headBlocked(1): NULL for an existent queue   ");
+    // if (q != procp[9])
+    //     adderrbuf("headBlocked(1): wrong process returned   ");
+    // p = outBlocked(q);
+    // if (p != q)
+    //     adderrbuf("outBlocked(1): couldn't remove from valid queue   ");
+    // q = headBlocked(&sem[9]);
+    // if (q == NULL)
+    //     adderrbuf("headBlocked(2): NULL for an existent queue   ");
+    // if (q != procp[19])
+    //     adderrbuf("headBlocked(2): wrong process returned   ");
+    // p = outBlocked(q);
+    // if (p != q)
+    //     adderrbuf("outBlocked(2): couldn't remove from valid queue   ");
+    // p = outBlocked(q);
+    // if (p != NULL)
+    //     adderrbuf("outBlocked: removed same process twice.");
+    // if (headBlocked(&sem[9]) != NULL)
+    //     adderrbuf("out/headBlocked: unexpected nonempty queue   ");
+
+    // for (i = 0; i < MAXPROC; i++)
+    //     freePcb(procp[i]);
+
+    // addokbuf("headBlocked and outBlocked ok   \n");
+    // addokbuf("ASH module ok   \n");
+
+    /* check Namespaces */
+    initNamespaces();
+    addokbuf("Initialized Namespaces\n");
+
+    /* check normal namespace (getNamespace) */
+    addokbuf("getNamespace test #1 started  \n");
+    for (i = 0; i < MAXPROC; i++) {
+        procp[i] = allocPcb();
+        if (getNamespace(procp[i], NS_PID) != NULL)
+            adderrbuf("getNamespace(1): unexpected Namespace   ");
+    }
+
+    addokbuf("getNamespace test #1 ok\n");
     // addokbuf("addNamespace test #1 started\n");
     // pid_ns = allocNamespace(NS_PID);
     // if (pid_ns == NULL)
