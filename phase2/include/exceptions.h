36241324ecb2c605336dcbe2137dc303690def6b
Nicole <nicolesabrina.marro@studio.unibo.it>
2023-08-19 16:46:07 +0200

    exception handler abbozzato

--- phase2/include/exceptions.h
+++ phase2/include/exceptions.h
@@ -1,2 +1,7 @@
-
+#include "pandos_const.h"
+#include "pandos_types.h"
+#include "types.h"
+#include "pcb.h"
+#include <umps/libumps.h>
+#include "scheduler.h"
 void exceptionHandler();
--- phase2/src/exceptions.c
+++ phase2/src/exceptions.c
@@ -1,4 +1,46 @@
 #include "include/exceptions.h"
-void exceptionHandler(){
+
+extern pcb_t *currentProcess;
+void exceptionHandler()
+{
+
+    // prendo la causa dell'eccezione e shifto i bit di 2
+    int cause = getCAUSE() & GETEXECCODE;
+    cause = cause >> CAUSESHIFT;
+
+    switch (cause)
+    {
+    case 0:
+        // interrupt handler
+        interruptHandler();
+        break;
+    case 1 ... 3:
+        // TLB exception handling
+        passupordie(PGFAULTEXCEPT);
+        break;
+
+    case 8:
+        // SYSCALL exception handler
+        syscallHandler();
+        break;
+
+    // da 4 a 7 e da 9 a 12
+    default:
+        // program trap exception handler
+        passupordie(GENERALEXCEPT);
+        break;
+    }
+}
+
+void passupordie(int index)
+{
+    //ricontrollare la &
+    if (&(currentProcess->p_supportStruct) == NULL)
+    {
+        //probabilmente sbagliata
+        terminate_process();
+
+        //SYSCALL(2, &(currentProcess->p_pid), 0,0);
+    }
     
 }
