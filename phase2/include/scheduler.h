4e3cdec6c606b2e1a2edda38028ae07e0a50c20f
annalisapol <annapol2003@gmail.com>
2023-08-19 10:39:07 +0200

    iniziato scheduler

--- phase2/include/scheduler.h
+++ phase2/include/scheduler.h
@@ -1,4 +1,5 @@
 #include "pandos_const.h"
 #include "pandos_types.h"
 #include "types.h"
+#include "pcb.h"
 #define NUM_SEMAPHORES (DEVINTNUM + 1) * DEVPERINT + 1
--- phase2/src/scheduler.c
+++ phase2/src/scheduler.c
@@ -1,4 +1,8 @@
 #include "include/scheduler.h"
+#define CONTINUECASE 1
+#define HALTCASE 2
+#define WAITCASE 3
+#define PANICCASE 4
 
 extern int processCount;
 extern int softBlockCount;
@@ -6,3 +10,35 @@ extern struct list_head readyQueue;
 extern pcb_t* currentProcess;
 extern int subDeviceSemaphores[NUM_SEMAPHORES];
 
+int check_case(){
+    if(!emptyProcQ(&readyQueue)){
+        return CONTINUECASE;
+    }
+    else if(processCount == 0){
+        return HALTCASE;
+    }
+    else if(processCount > 0 && softBlockCount > 0){
+        return WAITCASE;
+    }
+    else{
+        return PANICCASE;
+    }
+}
+
+void scheduler(){
+    int cases = check_case();
+    switch(cases){
+        case CONTINUECASE:
+            
+            break;
+        case HALTCASE:
+            HALT();
+            break;
+        case WAITCASE:
+            WAIT();
+            break;
+        case PANICCASE:
+            PANIC();
+            break;
+    }
+}
